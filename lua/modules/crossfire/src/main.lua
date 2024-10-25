6e3108b72ca497f18ba48005f0f5a9a5f7cc9dee
Rob Thomson <rob@marotori.com>
2024-10-25 11:44:54 +0100

    Crossfire tested and working

--- lua/modules/crossfire/src/main.lua
+++ lua/modules/crossfire/src/main.lua
@@ -23,7 +23,7 @@ local function close()
 end
 
 local function init()
-        system.registerElrsModule({configure = {name = config.moduleName, create = create, wakeup = wakeup, event = event, close = close}})
+        system.registerCrossfireModule({configure = {name = config.moduleName, create = create, wakeup = wakeup, event = event, close = close}})
 end
 
 return {init = init}
