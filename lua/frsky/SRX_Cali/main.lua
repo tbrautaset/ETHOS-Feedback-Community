4ca7ae48a4cc4ca4801c9c3c084babb8f33d1c9f
Bertrand Songis <bsongis@gmail.com>
2024-11-15 11:15:12 +0100

    Update the SRX Calibration script to be compatible with 1.5 and 1.6 (Device Manager)

--- lua/frsky/SRX_Cali/main.lua
+++ lua/frsky/SRX_Cali/main.lua
@@ -32,7 +32,7 @@ local function create()
   step = 0
   calibrationState = CALIBRATION_INIT
 
-  local sensor = sport.getSensor(0x0c30);
+  local sensor = sport.getSensor(0x0C30);
 
   local moduleLine = form.addLine("Module")
   local module = form.addChoiceField(moduleLine, nil, {{"Internal", 0x00}, {"External", 0x01}},
@@ -41,7 +41,7 @@ local function create()
       external = value
     end)
   moduleLine = form.addLine("")
-  form.addTextButton(moduleLine, nil, "CALIBRATE",
+  form.addTextButton(moduleLine, nil, "Calibrate",
     function()
       open = true
       module:enable(false)
@@ -50,7 +50,7 @@ local function create()
       end
     end)
 
-  bitmap = lcd.loadBitmap("/scripts/SRX_Cali/cali_"..step..".png")
+  bitmap = lcd.loadBitmap("cali_"..step..".png")
 
   return {sensor=sensor}
 end
@@ -63,7 +63,7 @@ local function paint(widget)
   else
     lcd.drawText(width / 2, height / 3, RB25_CALI_HINTS[step + 1], CENTERED)
     if calibrationState == CALIBRATION_INIT then
-      lcd.drawText(width / 2, height / 3 + 25, "Press CALIBRATE to start", CENTERED)
+      lcd.drawText(width / 2, height / 3 + 25, "Press Calibrate to start", CENTERED)
     else
       lcd.drawText(width / 2, height / 3 + 25, "Waiting...", CENTERED)
     end
@@ -76,44 +76,49 @@ local function paint(widget)
 end
 
 local function wakeup(widget)
-    if external then
-      widget.sensor:module(0x01)
+  if external then
+    widget.sensor:module(0x01)
+  end
+  if calibrationState == CALIBRATION_WRITE then
+    print("CALIBRATION_WRITE")
+    if widget.sensor:writeParameter(0x60, step) == true then
+      calibrationState = CALIBRATION_READ
+      lcd.invalidate()
     end
-    if calibrationState == CALIBRATION_WRITE then
-      print("CALIBRATION_WRITE")
-      if widget.sensor:writeParameter(0x60, step) == true then
-        calibrationState = CALIBRATION_READ
+  elseif calibrationState == CALIBRATION_READ then
+    print("CALIBRATION_READ")
+    if widget.sensor:requestParameter(0x60) == true then
+      calibrationState = CALIBRATION_WAIT
+    end
+  elseif calibrationState == CALIBRATION_WAIT then
+    local value = widget.sensor:getParameter()
+    if value then
+      local fieldId = value % 256
+      if fieldId == 0x60 then
+        if step == 5 then
+          calibrationState = CALIBRATION_OK
+          bitmap = lcd.loadBitmap("cali_ok.png")
+        else
+          calibrationState = CALIBRATION_INIT
+          step = (step + 1) % 6
+          bitmap = lcd.loadBitmap("cali_"..step..".png")
+        end
         lcd.invalidate()
       end
-    elseif calibrationState == CALIBRATION_READ then
-      print("CALIBRATION_READ")
-      if widget.sensor:requestParameter(0x60) == true then
-        calibrationState = CALIBRATION_WAIT
-      end
-    elseif calibrationState == CALIBRATION_WAIT then
-      local value = widget.sensor:getParameter()
-      if value then
-        local fieldId = value % 256
-        if fieldId == 0x60 then
-          if step == 5 then
-            calibrationState = CALIBRATION_OK
-            bitmap = lcd.loadBitmap("/scripts/SRX_Cali/cali_ok.png")
-          else
-            calibrationState = CALIBRATION_INIT
-            step = (step + 1) % 6
-            bitmap = lcd.loadBitmap("/scripts/SRX_Cali/cali_"..step..".png")
-          end
-          lcd.invalidate()
-        end
-      end
     end
---  end
+  end
 end
 
 local icon = lcd.loadMask("srx.png")
 
 local function init()
-  system.registerSystemTool({name=name, icon=icon, create=create, paint=paint, wakeup=wakeup})
+  if system.registerDeviceConfig then
+    system.registerDeviceConfig({category=DEVICE_CATEGORY_RECEIVERS, name="ESC", bitmap=icon, appId=0x0C30, pages={
+      {name=name, create=create, paint=paint, wakeup=wakeup}
+    }, version="1.5.19"})
+  else
+    system.registerSystemTool({name=name, icon=icon, create=create, paint=paint, wakeup=wakeup})
+  end
 end
 
 return {init=init}
