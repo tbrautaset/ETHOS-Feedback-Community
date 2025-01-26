63f33ae3443ee9431fc06a4ab67aad93dd03cbed
Nicholas-Luoyi <18351579360@163.com>
2025-01-26 14:40:29 +0800

    Reduce the ram usage

--- lua/frsky/StabilizerConfig/i18n/i18n.lua
+++ lua/frsky/StabilizerConfig/i18n/i18n.lua
@@ -2,20 +2,13 @@ local locale = system.getLocale()
 print("Get system language flag: ", locale)
 
 local I18nMap = {
-  cs = assert(loadfile(GlobalPath .. "i18n/cs.lua"))(),
-  de = assert(loadfile(GlobalPath .. "i18n/de.lua"))(),
   en = assert(loadfile(GlobalPath .. "i18n/en.lua"))(),
-  es = assert(loadfile(GlobalPath .. "i18n/es.lua"))(),
-  fr = assert(loadfile(GlobalPath .. "i18n/fr.lua"))(),
-  it = assert(loadfile(GlobalPath .. "i18n/it.lua"))(),
-  nl = assert(loadfile(GlobalPath .. "i18n/nl.lua"))(),
-  no = assert(loadfile(GlobalPath .. "i18n/no.lua"))(),
-  pb = assert(loadfile(GlobalPath .. "i18n/pb.lua"))(),
-  pl = assert(loadfile(GlobalPath .. "i18n/pl.lua"))(),
-  pt = assert(loadfile(GlobalPath .. "i18n/pt.lua"))(),
 }
 
 local function translate(key, paramTable)
+  if I18nMap[locale] == nil then
+    I18nMap[locale] = assert(loadfile(GlobalPath .. "i18n/" .. locale .. ".lua"))()
+  end
   local map = I18nMap[locale] or I18nMap['en']
   local string = map[key] or I18nMap['en'][key]
   if paramTable ~= nil and type(paramTable) == 'table' then
