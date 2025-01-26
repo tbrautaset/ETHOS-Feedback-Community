63e9ebcc4a885263925d4d79b9b17d76c3dcaa1a
Nicholas-Luoyi <18351579360@163.com>
2025-01-26 14:15:01 +0800

    Template for other languages

--- lua/frsky/StabilizerConfig/i18n/i18n.lua
+++ lua/frsky/StabilizerConfig/i18n/i18n.lua
@@ -2,7 +2,17 @@ local locale = system.getLocale()
 print("Get system language flag: ", locale)
 
 local I18nMap = {
+  cs = assert(loadfile(GlobalPath .. "i18n/cs.lua"))(),
+  de = assert(loadfile(GlobalPath .. "i18n/de.lua"))(),
   en = assert(loadfile(GlobalPath .. "i18n/en.lua"))(),
+  es = assert(loadfile(GlobalPath .. "i18n/es.lua"))(),
+  fr = assert(loadfile(GlobalPath .. "i18n/fr.lua"))(),
+  it = assert(loadfile(GlobalPath .. "i18n/it.lua"))(),
+  nl = assert(loadfile(GlobalPath .. "i18n/nl.lua"))(),
+  no = assert(loadfile(GlobalPath .. "i18n/no.lua"))(),
+  pb = assert(loadfile(GlobalPath .. "i18n/pb.lua"))(),
+  pl = assert(loadfile(GlobalPath .. "i18n/pl.lua"))(),
+  pt = assert(loadfile(GlobalPath .. "i18n/pt.lua"))(),
 }
 
 local function translate(key, paramTable)
